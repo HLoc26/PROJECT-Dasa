bee0a258f8bf0fbcec6d297fd633487cc00032cb
HLoc26 <117160235+HLoc26@users.noreply.github.com>
2023-11-07 13:10:35 +0700

    Init DLList header file

---
+++ DLList.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CauHoi.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Danh sách liên kết đôi chứa các câu hỏi đưọc chọn
+struct DLList {
+    CauHoi *head;
+    CauHoi *tail;
+    int size;
+
+    void Push(CauHoi ch) {}
+
+    void Pop() {}
+};
+typedef struct DLList DLList;
